91576a4a85c2cf5ab938a200049cfe495be9983d
Aditya Tomar <adityatomar@Adityas-MacBook-Air-3.local>
2025-06-25 01:57:11 +0530

    test file added

---
+++ monolith-app/backend/__tests__/app.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /api/products', () => {
+  it('should return an array of products (can be empty)', async () => {
+    const res = await request(app).get('/api/products');
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    
+    if (res.body.length > 0) {
+      expect(res.body[0]).toHaveProperty('name');
+      expect(res.body[0]).toHaveProperty('price');
+    }
+  });
+});
