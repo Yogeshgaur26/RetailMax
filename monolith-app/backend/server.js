73f5effffb0a7e3d5d79c859110a9ec1102ac63f
Aditya Tomar <143313634+adityatomar25@users.noreply.github.com>
2025-07-02 00:05:03 +0530

    logic updation

--- monolith-app/backend/server.js
+++ monolith-app/backend/server.js
@@ -1,13 +1,13 @@
-const express = require('express'); // ✅ this is required
+const express = require('express'); 
 const app = require('./app');
 const path = require('path');
 const PORT = 3000;
 
 // Serve static frontend files
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 app.listen(PORT, () => {
